d170d3a8e94b1e9b4b287bcd4df03a72699261c4
leoj3n <leoj3n@gmail.com>
2022-03-14 15:41:34 -0500

    1 binaries uploaded

---
+++ dist/assets/index.f8276018.js
@@ -0,0 +1,8 @@
+const s=function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}};s();const l=new URL("/assets/style2.8faaa29b.css",self.location).href;document.querySelector("#app").innerHTML=`
+  <!-- Cat here for "npm run dev" and "npm run build && npm run preview" -->
+  <div id="one"></div>
+
+  <!-- Cat NOT here when "npm run build && npm run preview" -->
+  <link rel="stylesheet" href="${l}">
+  <div id="two"></div>
+`;
