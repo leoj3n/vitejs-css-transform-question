b867e948b84034a64d68855a6b6b622b650a00b5
leoj3n <leoj3n@gmail.com>
2022-03-14 14:42:57 -0500

    1 file modified

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const url = new URL('./style2.css', import.meta.url).href;
-
 import './style.css';
 
+// After build, background url in style2.css does not point to hashed asset.
+const url = new URL('./style2.css', import.meta.url).href;
+
 document.querySelector('#app').innerHTML = `
-  <link rel="stylesheet" href="${url}">
+  <!-- Cat here for "npm run dev" and "npm run build && npm run preview" -->
   <div id="one"></div>
+
+  <!-- Cat NOT here when "npm run build && npm run preview" -->
+  <link rel="stylesheet" href="${url}">
   <div id="two"></div>
 `;
